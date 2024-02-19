d7ad75915e4851400a160d1492c700fbd6d69942
dnkmmr <dnkmmr69420@gmail.com>
2024-02-19 15:28:37 -0600

    fix code

--- main.go
+++ main.go
@@ -1,124 +1,97 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"os"
 	"os/exec"
-	"strings"
+	"path/filepath"
+	"runtime"
 )
 
-func downloadFile(url, fileName string, useCurl bool) error {
-	if useCurl {
-		return downloadFileCurl(url, fileName)
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <url>")
+		return
 	}
 
-	u, err := url.Parse(url)
+	fileURL := os.Args[1]
+	err := downloadFile(fileURL)
+	if err != nil {
+		fmt.Printf("Error downloading file: %v\n", err)
+		return
+	}
+
+	filePath, err := filepath.Abs(filepath.Base(fileURL))
+	if err != nil {
+		fmt.Printf("Error getting absolute file path: %v\n", err)
+		return
+	}
+
+	err = makeExecutable(filePath)
+	if err != nil {
+		fmt.Printf("Error making file executable: %v\n", err)
+		return
+	}
+
+	err = runFile(filePath)
+	if err != nil {
+		fmt.Printf("Error running file: %v\n", err)
+		return
+	}
+}
+
+func downloadFile(fileURL string) error {
+	u, err := url.Parse(fileURL)
 	if err != nil {
 		return err
 	}
 
-	switch u.Scheme {
-	case "http", "https":
-		return downloadFileHTTP(url, fileName)
-	case "ftp":
-		return downloadFileFTP(url, fileName)
-	default:
-		return fmt.Errorf("unsupported protocol: %s", u.Scheme)
+	filename := filepath.Base(u.Path)
+	if filename == "" {
+		filename = "downloaded_file"
 	}
-}
 
-func downloadFileHTTP(url, fileName string) error {
-	resp, err := http.Get(url)
+	out, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	var resp *http.Response
+	if u.Scheme == "ftp" {
+		resp, err = ftpDownload(fileURL)
+	} else {
+		resp, err = http.Get(fileURL)
+	}
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	file, err := os.Create(fileName)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
 
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		return err
-	}
+func ftpDownload(fileURL string) (*http.Response, error) {
+	// Implement FTP download logic here
+	// This is just a placeholder function
+	return nil, fmt.Errorf("FTP download not implemented")
+}
 
+func makeExecutable(filePath string) error {
+	if runtime.GOOS != "windows" {
+		return os.Chmod(filePath, 0755)
+	}
 	return nil
 }
 
-func downloadFileFTP(url, fileName string) error {
-	// Implement FTP download logic using Go's FTP library or your preferred FTP library.
-	return fmt.Errorf("FTP download not implemented")
-}
-
-func downloadFileCurl(url, fileName string) error {
-	cmd := exec.Command("curl", "-o", fileName, url)
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("error executing curl command: %v", err)
-	}
-
-	return nil
-}
-
-func makeExecutable(fileName string) error {
-	err := os.Chmod(fileName, 0755)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func runFile(fileName string) error {
-	cmd := exec.Command(fileName)
+func runFile(filePath string) error {
+	cmd := exec.Command(filePath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		return err
-	}
 
-	return nil
-}
-
-func main() {
-	useCurl := flag.Bool("c", false, "Use curl for downloading files")
-	flag.Parse()
-
-	args := flag.Args()
-	if len(args) < 2 {
-		fmt.Println("Please provide the URL and the local file name as arguments.")
-		return
-	}
-
-	url := args[0]
-	fileName := args[1]
-
-	err := downloadFile(url, fileName, *useCurl)
-	if err != nil {
-		fmt.Println("Error downloading file:", err)
-		if *useCurl && strings.Contains(err.Error(), "executable file not found") {
-			fmt.Println("Please make sure 'curl' is installed and available in your system's PATH.")
-		}
-		return
-	}
-
-	err = makeExecutable(fileName)
-	if err != nil {
-		fmt.Println("Error making file executable:", err)
-		return
-	}
-
-	err = runFile(fileName)
-	if err != nil {
-		fmt.Println("Error running file:", err)
-		return
-	}
+	return cmd.Run()
 }
